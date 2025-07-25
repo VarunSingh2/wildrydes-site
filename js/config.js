bd512c4769a17b9c9cb3833d09859a093663b7cd
VarunSingh2 <102214975+VarunSingh2@users.noreply.github.com>
2025-07-25 12:10:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_6wM2IlU65', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '737h75qk55415ajkk9arv90omb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
