4426cdf936256a3f79c427e800747a533a7038be
VarunSingh2 <102214975+VarunSingh2@users.noreply.github.com>
2025-07-25 12:45:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_6wM2IlU65', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '737h75qk55415ajkk9arv90omb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_A5a5CTvGH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4s3v5evt9b2fejaa7m53p0aev4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
